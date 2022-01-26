4bb406dd9affdc3b39e51a8da3dbbc6b99479eac
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-26 14:11:31 +0200

    Done

--- P11-03-CoreData/ContentView.swift
+++ P11-03-CoreData/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List(students) { student in
+                Text(student.name ?? "Unknown")
+            }
+            
+            Button("Add") {
+                let firstNames = ["Anna", "Alex", "Makar", "Julia", "Serg"]
+                let lastNames = ["Kotov", "Volkov", "Bazar", "Gural", "Neseda"]
+                
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+            }
+        }
     }
 }
 
