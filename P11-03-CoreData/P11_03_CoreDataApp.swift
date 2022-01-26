3b6ed93e54b1b3af954d63e0597fc27a0e7f8c95
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-26 13:28:12 +0200

    Initial Commit

---
+++ P11-03-CoreData/P11_03_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  P11_03_CoreDataApp.swift
+//  P11-03-CoreData
+//
+//  Created by Vahtee Boo on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct P11_03_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
